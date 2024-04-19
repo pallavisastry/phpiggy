5ecf32c64ddbc95316e1fe34a1c6e2f3796d0db6
pallavisastry <mspallavi.sastry@gmail.com>
2024-04-19 14:08:42 -0700

    Initial commit

---
+++ src/App/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+// include __DIR__ . "/../Framework/App.php";
+// replace with the following *after* composer autoload is setup
+require __DIR__ . "/../../vendor/autoload.php";
+
+use Framework\App;
+
+$app = new App;
+
+return $app;
